yuvi/ffmpeg

commit 8e04ff439544fe42a53d0d4460fb97228088dbce

Make h264 decoder conform to requirements of mpegvideo's MPV_frame_start. Namely, that it should not be called if you are starting to decode a B frame without any reference pictures. Prevents an endless allocation cycle in MPV_frame_start that will end in picture buffer overflow and abort. Fixes roundup issue 216.
git-svn-id: svn://svn.mplayerhq.hu/ffmpeg/trunk@11214 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

libavcodec/h264.c
@@ -3911,6 +3911,11 @@ static int decode_slice_header(H264Context *h, H264Context *h0){
     h->slice_type= slice_type;
 
     s->pict_type= h->slice_type; // to make a few old func happy, it's wrong though
+    if (s->pict_type == B_TYPE && s->last_picture_ptr == NULL) {
+        av_log(h->s.avctx, AV_LOG_ERROR,
+               "B picture before any references, skipping\n");
+        return -1;
+    }
 
     pps_id= get_ue_golomb(&s->gb);
     if(pps_id>=MAX_PPS_COUNT){

commit 16c492a34e5f9b20d2d13a2c05f844df28100709

Fix problem with multithreaded decoding, introduced by r11214. Patch by Reinhard Nissl: rnissl gmx de
git-svn-id: svn://svn.mplayerhq.hu/ffmpeg/trunk@11275 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

libavcodec/h264.c
@@ -3849,7 +3849,7 @@ static int decode_slice_header(H264Context *h, H264Context *h0){
     h->slice_type= slice_type;
 
     s->pict_type= h->slice_type; // to make a few old func happy, it's wrong though
-    if (s->pict_type == B_TYPE && s->last_picture_ptr == NULL) {
+    if (s->pict_type == B_TYPE && s0->last_picture_ptr == NULL) {
         av_log(h->s.avctx, AV_LOG_ERROR,
                "B picture before any references, skipping\n");
         return -1;