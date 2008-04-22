yuvi/ffmpeg

commit 08a77bd5a8f1342e442f9c095574324d7811d3f6

Use bytestream functions for reading frame header.
git-svn-id: svn://svn.mplayerhq.hu/ffmpeg/trunk@12929 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

libavcodec/mimic.c
@@ -25,6 +25,7 @@
 
 #include "avcodec.h"
 #include "bitstream.h"
+#include "bytestream.h"
 #include "dsputil.h"
 
 #define MIMIC_HEADER_SIZE   20
@@ -297,8 +298,13 @@ static int mimic_decode_frame(AVCodecContext *avctx, void *data,
         return -1;
     }
 
-    width  = AV_RL16(buf + 4);
-    height = AV_RL16(buf + 6);
+    buf       += 2;
+    quality    = bytestream_get_le16(&buf);
+    width      = bytestream_get_le16(&buf);
+    height     = bytestream_get_le16(&buf);
+    buf       += 4;
+    is_pframe  = bytestream_get_le32(&buf);
+    num_coeffs = bytestream_get_le32(&buf);
 
     if(!ctx->avctx) {
         int i;
@@ -322,10 +328,6 @@ static int mimic_decode_frame(AVCodecContext *avctx, void *data,
         return -1;
     }
 
-    quality    = AV_RL16(buf + 2);
-    is_pframe  = AV_RL32(buf + 12);
-    num_coeffs = buf[16];
-
     if(is_pframe && !ctx->buf_ptrs[ctx->prev_index].data[0]) {
         av_log(avctx, AV_LOG_ERROR, "decoding must start with keyframe\n");
         return -1;
@@ -346,7 +348,7 @@ static int mimic_decode_frame(AVCodecContext *avctx, void *data,
         return AVERROR_NOMEM;
 
     ctx->dsp.bswap_buf((uint32_t*)ctx->swap_buf,
-                        (const uint32_t*) (buf + MIMIC_HEADER_SIZE),
+                        (const uint32_t*) buf,
                         swap_buf_size>>2);
     init_get_bits(&ctx->gb, ctx->swap_buf, swap_buf_size << 3);
 

commit 4883bdcec6161343019c6e911d594c5768531ba2

Fix regression introduced by r12929. num_coeffs is just one byte (or maybe two?). It would make no sense for num_coeffs to be as high as 2^32.
git-svn-id: svn://svn.mplayerhq.hu/ffmpeg/trunk@12931 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

libavcodec/mimic.c
@@ -293,7 +293,8 @@ static int mimic_decode_frame(AVCodecContext *avctx, void *data,
     height     = bytestream_get_le16(&buf);
     buf       += 4; /* some constant */
     is_pframe  = bytestream_get_le32(&buf);
-    num_coeffs = bytestream_get_le32(&buf);
+    num_coeffs = bytestream_get_byte(&buf);
+    buf       += 3; /* some constant */
 
     if(!ctx->avctx) {
         int i;