yuvi/ffmpeg

commit 664b7609b124a48bad7209df002d812a05875a8d

mpeg12: fix non-standard use of "extern inline"
Rename ff_mpeg1_decode_block_intra to mpeg1_decode_block_intra and
make it static.  Add ff_mpeg1_decode_block_intra as plain extern
function calling mpeg1_decode_block_intra.

git-svn-id: svn://svn.mplayerhq.hu/ffmpeg/trunk@18859 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

libavcodec/mpeg12.c
@@ -320,7 +320,7 @@ static int mpeg_decode_mb(MpegEncContext *s,
             }
         } else {
             for(i=0;i<6;i++) {
-                if (ff_mpeg1_decode_block_intra(s, *s->pblocks[i], i) < 0)
+                if (mpeg1_decode_block_intra(s, *s->pblocks[i], i) < 0)
                     return -1;
             }
         }
@@ -605,7 +605,7 @@ static int mpeg_decode_motion(MpegEncContext *s, int fcode, int pred)
     return val;
 }
 
-inline int ff_mpeg1_decode_block_intra(MpegEncContext *s,
+static inline int mpeg1_decode_block_intra(MpegEncContext *s,
                                DCTELEM *block,
                                int n)
 {
@@ -676,6 +676,13 @@ inline int ff_mpeg1_decode_block_intra(MpegEncContext *s,
     }
     s->block_last_index[n] = i;
    return 0;
+}
+
+int ff_mpeg1_decode_block_intra(MpegEncContext *s,
+                                DCTELEM *block,
+                                int n)
+{
+    return mpeg1_decode_block_intra(s, block, n);
 }
 
 static inline int mpeg1_decode_block_inter(MpegEncContext *s,

libavcodec/mpeg12.h
@@ -56,6 +56,6 @@ static inline int decode_dc(GetBitContext *gb, int component)
     return diff;
 }
 
-extern inline int ff_mpeg1_decode_block_intra(MpegEncContext *s, DCTELEM *block, int n);
+extern int ff_mpeg1_decode_block_intra(MpegEncContext *s, DCTELEM *block, int n);
 
 #endif /* AVCODEC_MPEG12_H */

commit 1f82fca5fdae235f3bc8713cfe11d44280edb78a

Add a forward declaration of mpeg1_decode_block_intra to fix compilation broken by r18859 for e.g. all gcc 4 compilers.
git-svn-id: svn://svn.mplayerhq.hu/ffmpeg/trunk@18861 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

libavcodec/mpeg12.c
@@ -49,6 +49,9 @@
 #define MB_PTYPE_VLC_BITS 6
 #define MB_BTYPE_VLC_BITS 6
 
+static inline int mpeg1_decode_block_intra(MpegEncContext *s,
+                              DCTELEM *block,
+                              int n);
 static inline int mpeg1_decode_block_inter(MpegEncContext *s,
                               DCTELEM *block,
                               int n);