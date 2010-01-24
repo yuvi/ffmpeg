yuvi/ffmpeg

commit 373da7046c754453217e1f4ab0cef0e660c89a9a

make mpeg decoder handle mid-stream changes in resolution patch by Heikki Lindholm holindho at cs helsinki fi original thread: [Ffmpeg-devel] [PATCH] fix mid-stream resolution changes date: Sat Mar 31 13:52:50 CEST 2007
git-svn-id: svn://svn.mplayerhq.hu/ffmpeg/trunk@8865 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

libavcodec/mpeg12.c
@@ -2084,6 +2084,7 @@ typedef struct Mpeg1Context {
     int slice_count;
     int swap_uv;//indicate VCR2
     int save_aspect_info;
+    int save_width, save_height;
     AVRational frame_rate_ext;       ///< MPEG-2 specific framerate modificator
 
 } Mpeg1Context;
@@ -2138,6 +2139,8 @@ static int mpeg_decode_postinit(AVCodecContext *avctx){
         (s1->mpeg_enc_ctx_allocated == 0)||
         avctx->coded_width  != s->width ||
         avctx->coded_height != s->height||
+        s1->save_width != s->width ||
+        s1->save_height != s->height ||
         s1->save_aspect_info != s->aspect_ratio_info||
         0)
     {
@@ -2155,6 +2158,8 @@ static int mpeg_decode_postinit(AVCodecContext *avctx){
         avcodec_set_dimensions(avctx, s->width, s->height);
         avctx->bit_rate = s->bit_rate;
         s1->save_aspect_info = s->aspect_ratio_info;
+        s1->save_width = s->width;
+        s1->save_height = s->height;
 
      //low_delay may be forced, in this case we will have B frames
      //that behave like P frames

commit 23010b5e6185b538bdea0d3d6be32c4baf09ef47

Fix decoding ATI VCR2 codec, sample in samples/V-codecs/ATI-VCR12/ATI/VCR2test.avi Software decoding was broken since r8865, when two new context variables were added, but not initialized in vcr2_init_sequence(). Another such context variable was added in r18381.
git-svn-id: svn://svn.mplayerhq.hu/ffmpeg/trunk@21438 9553f0bf-9b14-0410-a0b8-cfaf0461ba5b

libavcodec/mpeg12.c
@@ -2092,6 +2092,9 @@ static int vcr2_init_sequence(AVCodecContext *avctx)
     s->chroma_format = 1;
     s->codec_id= s->avctx->codec_id= CODEC_ID_MPEG2VIDEO;
     avctx->sub_id = 2; /* indicates MPEG-2 */
+    s1->save_width           = s->width;
+    s1->save_height          = s->height;
+    s1->save_progressive_seq = s->progressive_sequence;
     return 0;
 }
 