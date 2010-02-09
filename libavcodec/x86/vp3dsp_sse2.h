8f2409ff4d712a0957f6283d8e92472db4696160
David Conrad <lessen42@gmail.com>
2010-02-08 22:19:33 -0500

    Add forgotten declaration of 10-element IDCT

--- libavcodec/x86/vp3dsp_sse2.h
+++ libavcodec/x86/vp3dsp_sse2.h
@@ -27,5 +27,6 @@
 void ff_vp3_idct_sse2(int16_t *input_data);
 void ff_vp3_idct_put_sse2(uint8_t *dest, int line_size, DCTELEM *block);
 void ff_vp3_idct_add_sse2(uint8_t *dest, int line_size, DCTELEM *block);
+void ff_vp3_idct10_add_sse2(uint8_t *dest, int line_size, DCTELEM *block);
 
 #endif /* AVCODEC_X86_VP3DSP_SSE2_H */
