4d0895bfeaded6afe95586fea4ceb3ba6cd7df71
David Conrad <lessen42@gmail.com>
2010-05-27 04:38:37 -0400

    Rename first to luma_start

--- libavcodec/vp8.c
+++ libavcodec/vp8.c
@@ -429,7 +429,7 @@ static void decode_block_coeffs(VP8Context *s, VP56RangeCoder *c, DCTELEM block[
 
 static void decode_mb_coeffs(VP8Context *s, VP56RangeCoder *c, VP8Macroblock *mb, DCTELEM block[6][4][16])
 {
-    int i, j, first = 0, luma_ctx = 3;
+    int i, j, luma_start = 0, luma_ctx = 3;
 
     s->dsp.clear_blocks((DCTELEM *)block);
 
@@ -442,14 +442,14 @@ static void decode_mb_coeffs(VP8Context *s, VP56RangeCoder *c, VP8Macroblock *mb
         // decode DC values and do hadamard
         decode_block_coeffs(s, c, dc, 0, s->prob.token[1]);
         s->dsp.vp8_luma_dc_wht(block, dc);
-        first = 1;
+        luma_start = 1;
         luma_ctx = 0;
     }
 
     // luma blocks
     for (i = 0; i < 4; i++)
         for (j = 0; j < 4; j++)
-            decode_block_coeffs(s, c, block[i][j], first, s->prob.token[luma_ctx]);
+            decode_block_coeffs(s, c, block[i][j], luma_start, s->prob.token[luma_ctx]);
 
     // chroma blocks
     for (i = 4; i < 6; i++)
