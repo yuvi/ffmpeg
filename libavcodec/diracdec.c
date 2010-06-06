7d6a31c9d35900643b8d48222d36c5e32d0a4982
David Conrad <lessen42@gmail.com>
2010-06-06 15:34:29 -0400

    Move unpack_idwt_params up

--- libavcodec/diracdec.c
+++ libavcodec/diracdec.c
@@ -873,6 +873,72 @@ static int dirac_unpack_prediction_parameters(DiracContext *s)
     return 0;
 }
 
+static int dirac_unpack_idwt_params(DiracContext *s)
+{
+    GetBitContext *gb = &s->gb;
+    int i, level;
+
+    align_get_bits(gb);
+
+    s->zero_res = s->num_refs ? get_bits1(gb) : 0;
+    if (s->zero_res)
+        return 0;
+
+    s->wavelet_idx = svq3_get_ue_golomb(gb);
+    if (s->wavelet_idx > 6)
+        return -1;
+
+    s->wavelet_depth = svq3_get_ue_golomb(gb);
+    if (s->wavelet_depth > MAX_DWT_LEVELS) {
+        av_log(s->avctx, AV_LOG_ERROR, "too many dwt decompositions\n");
+        return -1;
+    }
+
+    if (!s->low_delay) {
+        /* Codeblock paramaters (core syntax only) */
+        if (get_bits1(gb)) {
+            for (i = 0; i <= s->wavelet_depth; i++) {
+                s->codeblock[i].width = svq3_get_ue_golomb(gb);
+                s->codeblock[i].height = svq3_get_ue_golomb(gb);
+            }
+
+            s->codeblock_mode = svq3_get_ue_golomb(gb);
+            if (s->codeblock_mode > 1) {
+                av_log(s->avctx, AV_LOG_ERROR, "unknown codeblock mode\n");
+                return -1;
+            }
+        } else
+            for (i = 0; i <= s->wavelet_depth; i++)
+                s->codeblock[i].width = s->codeblock[i].height = 1;
+    } else {
+        s->lowdelay.num_x     = svq3_get_ue_golomb(gb);
+        s->lowdelay.num_y     = svq3_get_ue_golomb(gb);
+        s->lowdelay.bytes.num = svq3_get_ue_golomb(gb);
+        s->lowdelay.bytes.den = svq3_get_ue_golomb(gb);
+
+        if (get_bits1(gb)) {
+            // custom quantization matrix
+            s->lowdelay.quant[0][0] = svq3_get_ue_golomb(gb);
+            for (level = 0; level < s->wavelet_depth; level++) {
+                s->lowdelay.quant[level][1] = svq3_get_ue_golomb(gb);
+                s->lowdelay.quant[level][2] = svq3_get_ue_golomb(gb);
+                s->lowdelay.quant[level][3] = svq3_get_ue_golomb(gb);
+            }
+        } else {
+            // default quantization matrix
+            for (level = 0; level < s->wavelet_depth; level++)
+                for (i = 0; i < 4; i++) {
+                    s->lowdelay.quant[level][i] = default_qmat[s->wavelet_idx][level][i];
+
+                    // haar with no shift differs for different depths
+                    if (s->wavelet_idx == 3)
+                        s->lowdelay.quant[level][i] += 4*(s->wavelet_depth-1 - level);
+                }
+        }
+    }
+    return 0;
+}
+
 static inline int pred_sbsplit(uint8_t *sbsplit, int stride, int x, int y)
 {
     static const uint8_t avgsplit[7] = { 0, 0, 1, 1, 1, 2, 2 };
@@ -1090,72 +1156,6 @@ static void dirac_unpack_block_motion_data(DiracContext *s)
                     propagate_block_data(block, s->blwidth, step);
                 }
         }
-}
-
-static int dirac_unpack_idwt_params(DiracContext *s)
-{
-    GetBitContext *gb = &s->gb;
-    int i, level;
-
-    align_get_bits(gb);
-
-    s->zero_res = s->num_refs ? get_bits1(gb) : 0;
-    if (s->zero_res)
-        return 0;
-
-    s->wavelet_idx = svq3_get_ue_golomb(gb);
-    if (s->wavelet_idx > 6)
-        return -1;
-
-    s->wavelet_depth = svq3_get_ue_golomb(gb);
-    if (s->wavelet_depth > MAX_DWT_LEVELS) {
-        av_log(s->avctx, AV_LOG_ERROR, "too many dwt decompositions\n");
-        return -1;
-    }
-
-    if (!s->low_delay) {
-        /* Codeblock paramaters (core syntax only) */
-        if (get_bits1(gb)) {
-            for (i = 0; i <= s->wavelet_depth; i++) {
-                s->codeblock[i].width = svq3_get_ue_golomb(gb);
-                s->codeblock[i].height = svq3_get_ue_golomb(gb);
-            }
-
-            s->codeblock_mode = svq3_get_ue_golomb(gb);
-            if (s->codeblock_mode > 1) {
-                av_log(s->avctx, AV_LOG_ERROR, "unknown codeblock mode\n");
-                return -1;
-            }
-        } else
-            for (i = 0; i <= s->wavelet_depth; i++)
-                s->codeblock[i].width = s->codeblock[i].height = 1;
-    } else {
-        s->lowdelay.num_x     = svq3_get_ue_golomb(gb);
-        s->lowdelay.num_y     = svq3_get_ue_golomb(gb);
-        s->lowdelay.bytes.num = svq3_get_ue_golomb(gb);
-        s->lowdelay.bytes.den = svq3_get_ue_golomb(gb);
-
-        if (get_bits1(gb)) {
-            // custom quantization matrix
-            s->lowdelay.quant[0][0] = svq3_get_ue_golomb(gb);
-            for (level = 0; level < s->wavelet_depth; level++) {
-                s->lowdelay.quant[level][1] = svq3_get_ue_golomb(gb);
-                s->lowdelay.quant[level][2] = svq3_get_ue_golomb(gb);
-                s->lowdelay.quant[level][3] = svq3_get_ue_golomb(gb);
-            }
-        } else {
-            // default quantization matrix
-            for (level = 0; level < s->wavelet_depth; level++)
-                for (i = 0; i < 4; i++) {
-                    s->lowdelay.quant[level][i] = default_qmat[s->wavelet_idx][level][i];
-
-                    // haar with no shift differs for different depths
-                    if (s->wavelet_idx == 3)
-                        s->lowdelay.quant[level][i] += 4*(s->wavelet_depth-1 - level);
-                }
-        }
-    }
-    return 0;
 }
 
 static int weight(int i, int blen, int offset)
